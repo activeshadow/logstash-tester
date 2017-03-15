c48c5c7530edd999275bddf6f284e1edd572bfdf
Bryan Richardson <bryan@activeshadow.com>
2017-03-15 08:53:38 -0600

    Updated filter spec to handle multiple outputs for a single event
    This might happen when the Logstash `split` function is used to create
    multiple events from a single event based on a particular field.
    
    Rather than just taking `result[0]` in the filter spec, it now loops
    through all the results returned and compares them to the expected
    output as defined in the JSON test file. When Logstash splits a single
    event into multiple events, most of the event fields are copied from the
    original to all the newly generated events. As such, the JSON test file
    should identify the fields that will have multiple results via an array
    of the results.
    
    For example:
    
        "cases": [{
          "in": "....",
          "out": {
            "type": "foo", # same for all events
            "message": ["bar", "fish"] # two events should exist
          }
        }]

--- test/spec/filter_spec.rb
+++ test/spec/filter_spec.rb
@@ -14,37 +14,64 @@ files.sort.each do |file|
   @@configuration << File.read(file)
 end
 
-
 def run_case(tcase, fields, ignore, data_file, i)
   input = fields
   input['message'] = tcase['in']
 
   msg_header = "[#{File.basename(data_file)}##{i}]"
 
-   sample(input) do
+  # The `sample` method is provided by
+  # logstash-devutils/lib/logstash/devutils/rspec/logstash_helpers.rb.
+  # The `results` variable in the block to `sample` will be the resulting
+  # event after being processed by any filters present in the configuration,
+  # which is initialized below in the call to `config` (also provided by
+  # logstash_helpers.rb). We clone the input here since tests are run in
+  # parallel.
+  sample(input.clone) do
     expected = tcase['out']
+
+    if expected.empty?
+      msg = "\n#{msg_header} Expected output to be empty.\nComplete logstash output: #{results}\n--"
+      expect(results).to be_empty, msg
+      next # exit early from block
+    end
+
+    if results.empty?
+      msg = "\n#{msg_header} No output, but expected.\nComplete expected output: #{expected}\n--"
+      expect(expected).to be_empty, msg
+      next # exit early from block
+    end
+
     expected_fields = expected.keys
 
-    # Handle no results (for example, when a line is voluntarily dropped)
-    lsresult = results.any? ? results[0] : {}
-    result_fields = lsresult.to_hash.keys.select { |f| not ignore.include?(f) }
+    results.each do |result|
+      fields = result.to_hash.keys.select { |f| not ignore.include?(f) }
 
-    # TODO test for grokparsefailures
+      # Test for presence of expected fields
+      missing = expected_fields.select { |f| not fields.include?(f) }
+      msg = "\n#{msg_header} Fields missing in logstash output: #{missing}\nComplete logstash output: #{result.to_hash}\n--"
+      expect(missing).to be_empty, msg
 
-    # Test for presence of expected fields
-    missing = expected_fields.select { |f| not result_fields.include?(f) }
-    msg = "\n#{msg_header} Fields missing in logstash output: #{missing}\nComplete logstash output: #{lsresult.to_hash}\n--"
-    expect(missing).to be_empty, msg
+      # Test for presence of unknown fields
+      extra = fields.select { |f| not expected_fields.include?(f) }
+      msg = "\n#{msg_header} Unexpected fields in logstash output: #{extra}\nComplete logstash output: #{result.to_hash}\n--"
+      expect(extra).to be_empty, msg
 
-    # Test for absence of unknown fields
-    extra = result_fields.select { |f| not expected_fields.include?(f) }
-    msg = "\n#{msg_header} Unexpected fields in logstash output: #{extra}\nComplete logstash output: #{lsresult.to_hash}\n--"
-    expect(extra).to be_empty, msg
+      # Test individual field values
+      expected.each do |name,value|
+        msg = "\n#{msg_header} Field value mismatch: '#{name}'\nExpected: #{value} (#{value.class})\nGot: #{result.get(name)} (#{result.get(name).class})\n\n--"
 
-    # Test individual field values
-    expected.each do |name,value|
-      msg = "\n#{msg_header} Field value mismatch: '#{name}'\nExpected: #{value} (#{value.class})\nGot: #{lsresult.get(name)} (#{lsresult.get(name).class})\n\n--"
-      expect(lsresult.get(name).to_s).to eq(value.to_s), msg
+        # If specified in the test case as an array (except for tags), check to
+        # see if current output is included in the array of expected outputs.
+        # TODO: track to make sure all elements in array of expected outputs are
+        # accounted for (perhaps by removing them from expected as they're
+        # matched?).
+        if value.is_a?(Array) and name != "tags" # hack! :(
+          expect(value.include?(result.get(name))).to be_truthy, msg
+        else
+          expect(result.get(name).to_s).to eq(value.to_s), msg
+        end
+      end
     end
   end
 end
